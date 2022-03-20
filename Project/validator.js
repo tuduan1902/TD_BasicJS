b59bcbb670f4fb09debc968c7b546a8740f0c658
tuduan1902 <uuxinhxinh2701@gmail.com>
2022-03-20 23:23:54 +0700

    commit

--- Project/validator.js
+++ Project/validator.js
@@ -1,13 +1,26 @@
 
 // Đối tượng `Validator`
 function Validator(options){
+
+    var selectorRules = {};
+
     // Hàm thực hiện validate
     function validate(inputElement, rule){
         // value: inputElement.value
         // test func: rules.test 
-        var errorMessage = rule.test(inputElement.value)
+        var errorMessage;
         // Từ inputElement gọi đến thẻ form-group rồi gọi tới form-message
         var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+
+        // Lặp qua từng rule & kiểm tra
+        // Nếu có lỗi thì dừng việc kiểm tra
+        for (var i = 0; i < rules.length; i++){
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
             if(errorMessage){
                 errorElement.innerText = errorMessage;
                 inputElement.parentElement.classList.add('invalid');
@@ -20,13 +33,29 @@ function Validator(options){
     // Lấy element của form cần validate
     var formElement = document.querySelector(options.form);
     if(formElement){
+        // Khi submit form
+        formElement.onsubmit = function(event){
+            event.preventDefault();
+        }
+
+
+        // Lặp qua mỗi rule và xử lý(lắng nghe sự kiện blur, input, ...)
         options.rules.forEach(function(rule){
+
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])){
+                selectorRules[rule.selector].push(rule.test);
+            }else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+              
+
             var inputElement = formElement.querySelector(rule.selector);
             
             if(inputElement){
                 // Xử lý trường hợp blur khỏi input
                 inputElement.onblur = function(){
-                    validate(inputElement,rule);
+                    validate(inputElement, rule);
                 }
 
                 // Xử lý mỗi khi người dùng nhập vào input
@@ -37,6 +66,7 @@ function Validator(options){
                 }
             }
         });
+
     }
 }
 
@@ -44,39 +74,39 @@ function Validator(options){
 // Nguyên tắc của các rules:
 // 1. Khi có lỗi => return message lỗi
 // 2. Khi hợp lệ => return undefined
-Validator.isRequired = function (selector){
+Validator.isRequired = function (selector, message){
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
         }
     };
 }
 
-Validator.isEmail = function (selector){
+Validator.isEmail = function (selector, message){
     return {
         selector: selector,
         test: function(value){
             var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            return regex.test(value) ? undefined : 'Vui lòng nhập email';
+            return regex.test(value) ? undefined : message || 'Vui lòng nhập email';
         }
     };
 }
 
-Validator.minLength = function (selector,min){
+Validator.minLength = function (selector, min, message){
     return {
         selector: selector,
         test: function(value){
-            return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự`;
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
         }
     };
 }
 
-Validator.isConfirmed = function (selector, getCofirmValue){
+Validator.isConfirmed = function (selector, getCofirmValue,  message){
     return {
         selector: selector,
         test: function (value) {
-            return value === getCofirmValue ? undefined : 'Giá trị nhập vào không chính xác'
+            return value === getCofirmValue ? undefined : message || 'Giá trị nhập vào không chính xác'
         }
     };
 }
